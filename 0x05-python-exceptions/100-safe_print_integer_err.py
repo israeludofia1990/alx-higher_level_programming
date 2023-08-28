59272e0a070aa8dcf4b2f140e9771dfcb10a579e
udofiaisrael1990 <udofiaisrael170@gmail.com>
2023-08-28 14:02:07 -0700

    100-safe_print_integer_err.py

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except Exception as error:
+        print("Exception: {}".format(error), file=sys.stderr)
+        return (False)
