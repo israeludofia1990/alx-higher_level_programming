53220bfcb51df37affcb3aa13a7583055460e245
udofiaisrael1990 <udofiaisrael170@gmail.com>
2023-08-30 08:03:07 -0700

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Module: Sqaure_Module """
+
+
+class Square:
+    """A class that defines a sqaure"""
+
+    def __init__(self, size=0):
+        """
+        Initializes a new square instance
+
+        Args:
+            size(int): Size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
