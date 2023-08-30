4fa3ab1fb54138b10a71e3f77a7a5b8a0deda2be
udofiaisrael1990 <udofiaisrael170@gmail.com>
2023-08-30 06:34:42 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Module: Sqaure_Module """
+
+
+class Square:
+    """A class that defines a sqaure"""
+
+    def __init__(self, size):
+        """
+        Initializes a new square instance
+
+        Args:
+            size(int): Size of the square
+        """
+        self.__size = size
