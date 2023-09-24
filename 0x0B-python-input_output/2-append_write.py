61d3cf4e755594412398a3c65eab99468e6ffffb
israeludofia1990 <61766214+israeludofia1990@users.noreply.github.com>
2023-09-24 22:35:15 +0100

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/2-append_write.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-'''contains a module that appends a text file'''
-
-
-def append_write(filename="", text=""):
-    '''Appends to a text file'''
-    with open(filename, 'a', encoding='utf-8') as my_file:
-        return my_file.write(text)
