a6c6563c53a332c35222382672c07103f45e18c6
udofiaisrael1990 <udofiaisrael170@gmail.com>
2023-11-15 09:31:24 -0800

    0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,5 @@
 -- lists all databases of your MySQL server
 
--- shows all databases on server
-SHOW DATABASES;
+-- lists all privileges of the MySQL users
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
