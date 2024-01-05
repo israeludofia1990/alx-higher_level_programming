1f02a2228b28cac90f5b41603d7c0f781ad83b34
udofiaisrael1990 <udofiaisrael170@gmail.com>
2024-01-05 12:42:28 +0000

    first commit

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#Bash script that takes in a URL, sends a request to
-#that URL, and displays the size of the body of the response
-curl -sI "$1" | grep -w 'Content-Length' | cut -f2 -d' ' 
+# script to get the body size of a request
+curl -Is "$1" | grep -w 'Content-Length' | cut -f2 -d' '
