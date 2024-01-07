38eba9eb7dcc8017d255f0a51c32e4f087be9c8e
udofiaisrael1990 <udofiaisrael170@gmail.com>
2024-01-07 09:53:33 -0800

    second commit

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Write a Python script that fetches https://alx-intranet.hbtn.io/status'''
+import requests
+
+if __name__ == "__main__":
+    response = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(response)))
+    print("\t- content: {}".format(response))
