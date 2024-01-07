8408cfd855308908f1f7a978db2996fa550c3354
udofiaisrael1990 <udofiaisrael170@gmail.com>
2024-01-07 09:04:46 -0800

    second commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
         html = resp.read()
-    print("Body response:")
-    print(f"    - type: {type(html)}")
-    print(f"    - content: {html}")
-    print(f"    - utf8 content: {html.decode('utf-8')}")
+        print('Body response:')
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 '''sends a POST request to the passed URL with the email as a parameter'''
-from sys import argv
-from urllib import request, parse
 
 if __name__ == "__main__":
+    from sys import argv
+    import urllib.parse
+    import urllib.request
+
     values = {'email': argv[2]}
-    data = parse.urlencode(values)
+    data = urllib.parse.urlencode(values)
     data = data.encode('utf-8')
-    req = request.Request(argv[1], data)
+    req = urllib.request.Request(argv[1], data)
     with urllib.request.urlopen(req) as response:
         print(response.read().decode('utf-8'))
